b7d3914fd0cb64aee31885b6812f4f9d7ed1234b
christothes <chriscott@hotmail.com>
2014-01-11 12:23:07 -0600

    better E2E test for lobby. More to come

--- e2e-test/e2eTest.js
+++ e2e-test/e2eTest.js
@@ -2,13 +2,17 @@
  * Created by chriss on 1/10/14.
  */
 describe('node against humanity lobby', function() {
-  it('should initialize with no active games', function() {
+  it('should display the noGames text only when no games are available', function() {
     browser.get('http://localhost:3000');
 
     var gameList = element.all(by.repeater('game in availableGames'));
 
     gameList.then(function(arr) {
-      expect(arr.length).toEqual(0);
+      if(arr.length === 0){
+        expect(element(by.id('noGames')).isDisplayed()).toBe(true);
+      } else {
+        expect(element(by.id('noGames')).isDisplayed()).toBe(false);
+      }
     })
   });
 });
---
+++ e2e-test/e2econf.js
@@ -0,0 +1,7 @@
+/**
+ * Created by chriss on 1/10/14.
+ */
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['*Test.js']
+}
