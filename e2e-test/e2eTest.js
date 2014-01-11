78c0645b815e9f3be7644cd0729d55cb84a47642
christothes <chriscott@hotmail.com>
2014-01-11 16:28:01 -0600

    Mores E2E tests + minor tweak to game.html

--- e2e-test/e2eTest.js
+++ e2e-test/e2eTest.js
@@ -13,6 +13,26 @@ describe('node against humanity lobby', function() {
       } else {
         expect(element(by.id('noGames')).isDisplayed()).toBe(false);
       }
-    })
+    });
+
+
+  });
+
+  it('should start the game with the players name when Create Game is clicked and name is entered', function() {
+    element(by.model('gameSvc.playerName')).sendKeys('Chris');
+    element(by.id('aCreatGame')).click();
+
+    expect(element(by.id('gameName')).getText()).toEqual('Chris\'s game (waiting for players)');
+
+    setTimeout(function() {
+      //give the page a second to return results and change the bindings. Not sure if there is a better way to do this
+      expect(element(by.id('notificationCardCzar')).isDisplayed()).toBe(true);
+      expect(element(by.id('notificationSelectCard')).isDisplayed()).toBe(false);
+      expect(element(by.id('notificationWaitingOnCzar')).isDisplayed()).toBe(false);
+      expect(element(by.id('notificationWaitingOnCards')).isDisplayed()).toBe(true);
+      expect(element(by.id('notificationSelectWinner')).isDisplayed()).toBe(false);
+      expect(element(by.id('notificationCardCzar')).isDisplayed()).toBe(false);
+    }, 1000)
+
   });
 });
