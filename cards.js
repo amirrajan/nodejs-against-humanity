eb857092c84bdc0320da0b967ed271b898ff3090
Jeffrey Oliver <jeffrey.oliver@panasonic.aero>
2015-06-13 22:53:19 -0700

    For now filter out the black cards except pick one.

--- cards.js
+++ cards.js
@@ -7,7 +7,10 @@ function getDeck() {
 function black() {
   //  Get the cards from the json database and filter them to work out.
   console.log(card_data.blackCards.length);
-  return card_data.blackCards;
+  function filter(v) {
+    return v.pick == 1;
+  }
+  return card_data.blackCards.filter(filter);
 }
 
 function white() {
